46e2e472793f284932ec45a668f275e6fed934ff
Marek Pankowski <17934750+mkpanq@users.noreply.github.com>
2025-09-04 15:41:55 +0200

    Small updated on the main page

--- src/app/(home)/page.tsx
+++ src/app/(home)/page.tsx
@@ -11,13 +11,13 @@ export default async function Home() {
       <Container className="mt-9 sm:mt-15">
         <div className="max-w-3xl">
           <h1 className="text-4xl font-bold tracking-tight text-zinc-800 sm:text-5xl dark:text-zinc-100">
-            Fullstack software engineer, creator, wannabe (sim)racing driver 🏎️
+            Hi ! I'm Marek 👋🏻 Fullstack product engineer & digital creator
+            👨🏻‍💻
           </h1>
           <p className="mt-6 text-base text-zinc-600 dark:text-zinc-400">
-            I'm Marek Pankowski, a fullstack software engineer and product
-            creator based in Warsaw, Poland. I build thoughtful software and
-            help teams focus on what matters — solid engineering and a
-            product-first mindset.
+            I'm a fullstack software engineer and product creator based in
+            Warsaw, Poland. I build thoughtful software and help teams focus on
+            what matters — solid engineering and a product-first mindset.
           </p>
           <div className="mt-6 flex gap-6">
             {Socials.map((social) => (
