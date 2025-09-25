6de26bceb307b58d1b10f1ba5ba30c72b90d5701
Marek Pankowski <17934750+mkpanq@users.noreply.github.com>
2025-09-25 16:45:47 +0200

    Simple debouncing

--- src/components/GardenBrowser.tsx
+++ src/components/GardenBrowser.tsx
@@ -3,30 +3,34 @@ import { useEffect, useState } from "react";
 import { Container } from "./Container";
 
 const GardenBrowser = () => {
+  const [inputSearchPhrase, setInputSearchPhrase] = useState("");
   const [searchPhrase, setSearchPhrase] = useState("");
 
   useEffect(() => {
-    setTimeout(() => {
-      console.log("Search phrase is: ", searchPhrase);
-    }, 3000);
+    const timeoutId = setTimeout(() => {
+      setSearchPhrase(inputSearchPhrase);
+    }, 1500);
+
+    return () => clearTimeout(timeoutId);
+  }, [inputSearchPhrase]);
+
+  useEffect(() => {
+    console.log("Search phrase is: ", searchPhrase);
   }, [searchPhrase]);
 
   return (
-    <>
-      <Container className="mt-15">
-        <div>
-          <input
-            id="name"
-            name="name"
-            type="text"
-            placeholder="<todo search encouraging>"
-            onChange={(event) => setSearchPhrase(event.target.value)}
-            className="block w-full rounded-2xl bg-white px-4 py-2 text-base text-gray-900 outline-2 -outline-offset-1 outline-gray-300 placeholder:text-gray-400 focus:outline-2 focus:-outline-offset-2 focus:outline-indigo-600 sm:text-sm/6"
-          />
-        </div>
-      </Container>
-      <p>{searchPhrase}</p>
-    </>
+    <Container className="mt-15">
+      <div>
+        <input
+          id="name"
+          name="name"
+          type="text"
+          placeholder="<todo search encouraging>"
+          onChange={(event) => setInputSearchPhrase(event.target.value)}
+          className="block w-full rounded-2xl bg-white px-4 py-2 text-base text-gray-900 outline-2 -outline-offset-1 outline-gray-300 placeholder:text-gray-400 focus:outline-2 focus:-outline-offset-2 focus:outline-indigo-600 sm:text-sm/6"
+        />
+      </div>
+    </Container>
   );
 };
 
