5f9a799b9b393e3e02c6311b2cf9bd9f645cacd3
Marek Pankowski <17934750+mkpanq@users.noreply.github.com>
2025-09-25 17:07:34 +0200

    TODOs for future

--- src/components/GardenBrowser.tsx
+++ src/components/GardenBrowser.tsx
@@ -1,4 +1,5 @@
 "use client";
+
 import { useEffect, useState } from "react";
 import { Container } from "./Container";
 
@@ -18,6 +19,9 @@ const GardenBrowser = () => {
     console.log("Search phrase is: ", searchPhrase);
   }, [searchPhrase]);
 
+  // DB with all links
+  // Searching mechanism - need to check how best and easy to approach it
+  // Display cards below - use React Query to handle the loading state
   return (
     <Container className="mt-15">
       <div>
@@ -27,8 +31,11 @@ const GardenBrowser = () => {
           type="text"
           placeholder="<todo search encouraging>"
           onChange={(event) => setInputSearchPhrase(event.target.value)}
-          className="block w-full rounded-2xl bg-white px-4 py-2 text-base text-gray-900 outline-2 -outline-offset-1 outline-gray-300 placeholder:text-gray-400 focus:outline-2 focus:-outline-offset-2 focus:outline-indigo-600 sm:text-sm/6"
+          className="block w-full rounded-2xl bg-white px-4 py-2 text-base text-gray-900 outline-2 -outline-offset-1 outline-gray-300 placeholder:text-gray-400 focus:outline-2 focus:-outline-offset-2 focus:outline-amber-600 sm:text-sm/6"
         />
+      </div>
+      <div>
+        <p>Result cards</p>
       </div>
     </Container>
   );
